1118c21fb224b94fc743bf7b652e71b3194b6f87
Jair Flores <jairflores0802@gmail.com>
2023-01-29 17:09:29 +0100

    Agregando funcionalidad

--- ejercicio1.6/index.js
+++ ejercicio1.6/index.js
@@ -7,13 +7,33 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = (props) => {
+    setGood (good + 1)
+  }
+
+  const handleNeutral = (props) => {
+    setNeutral (neutral + 1)
+  }
+
+  const handleBad = (props) => {
+    setBad (bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+
+      <h1>Statitics</h1>
+      <p>good :  {good}</p>
+      <p>neutral :  {neutral}</p>
+      <p>bad : {bad}</p>
+
     </div>
+
   )
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
