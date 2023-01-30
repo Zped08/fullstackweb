290a34fc37ddbf4761f1756513a6116ea5035d8e
Jair Flores <jairflores0802@gmail.com>
2023-01-30 19:23:59 +0100

    agregando la funcionalidad de anecdota con el mayor numero de votos

--- anecdota-app/src/index.js
+++ anecdota-app/src/index.js
@@ -3,25 +3,31 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
-  const[votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0))
+  const [votes, setVotes] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 })
 
-const handleClick = () => {
-  setSelected(Math.floor(Math.random() * props.anecdotes.length))
-}
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * props.anecdotes.length))
+  }
 
-const handleVote = () => { 
-  const copy = [...votes]
-  copy[selected] +=1
-  setVotes(copy)
-}
-return (
-  <div>
-    <p>{props.anecdotes[selected]}</p>
-    <p>{votes[selected]} votes</p>
-    <button onClick = {handleClick}> Next anecdote</button>
-    <button onClick = {handleVote}> vote</button>
-  </div>
-)
+  const handleVote = () => {
+    const copy = { ...votes }
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
+  const maxVotes = Math.max(...Object.values(votes))
+  const maxVotedAnecdote = props.anecdotes.find((anecdote, index) => votes[index] === maxVotes)
+
+  return (
+    <div>
+      <p>{maxVotedAnecdote}</p>
+      <p>has {maxVotes} votes</p>
+      <p>{props.anecdotes[selected]}</p>
+      <p>{votes[selected]} votes</p>
+      <button onClick={handleClick}> Next anecdote</button>
+      <button onClick={handleVote}> vote</button>
+    </div>
+  )
 }
 
 const anecdotes = [
@@ -33,4 +39,4 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
-ReactDOM.render(<App anecdotes={anecdotes} />,document.getElementById('root'))
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'))
